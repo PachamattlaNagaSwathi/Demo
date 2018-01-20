8827d98274aa7f7a2a0b13978fa8924f235c0934
Swathi <pachamattlaswathi@gmail.com>
2018-01-20 15:34:10 -0800

    Java File Added

---
+++ Example.java
@@ -0,0 +1,26 @@
+/* conditional : if, ifelse, switch
+iteration : for ,while, do while
+transfer : goto, break, continue, try, return*/
+
+class Example
+{
+	public static void main( String[] args)
+
+	{ 
+		int a = 6 , b=10, c=2;
+		if (a>b && a>c)
+		{
+			System.out.println(a);
+
+		}
+		else if (a<b && a<c)
+		
+
+			System.out.println(b);
+		else
+			System.out.println(c);
+
+
+	}
+
+}
